0965a790ffb85866d7545e32cc31ec5ae50cabe3
laogia201 <o0oking.dark@gmail.com>
2026-03-06 15:55:56 +0700

    fix limit data upload

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/files', require('./routes/fileRoutes'));
